c20cd6d831b06a4f7f82275156abaa15fdeffaa5
tjhindman <thind131@gmail.com>
2022-08-02 21:27:57 -0400

    ./dotnetProjects/CsharpFundamentals added including ../VariablesAndTypes c# project assembly. functionality for searching with 'enter' key also added in ./javascriptProjects/apiFundamentals

---
+++ dotnetProjects/CsharpFundamentals/VariablesAndTypes/Program.cs
@@ -0,0 +1,13 @@
+﻿// single line comment in C# (2 forward slashes)
+
+/*
+    multi line comment in C#
+
+    can put as many lines as you want
+*/
+
+/// XML COMMENTS
+/// these are used to add definition for specific items in your code
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
