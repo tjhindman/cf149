5bff25a033088e95326c011b82d2208ea747507a
tjhindman <thind131@gmail.com>
2022-08-25 19:39:22 -0400

    Drofsnar v1 complete added

--- challenges/Drofsnar/ProgramUI.cs
+++ challenges/Drofsnar/ProgramUI.cs
@@ -1,4 +1,3 @@
-using Internal;
 using System;
 public class ProgramUI {
     public void Run() {
@@ -34,18 +33,83 @@ public class ProgramUI {
 
                         hunterPts = hunterPts * 2;
                         break;
+                    case "Bird":
+                        Console.ForegroundColor = ConsoleColor.Blue;
+                        System.Console.WriteLine("It's a bird! 10 points added!");
+
+                        points += 10;
+                        System.Console.WriteLine($"Point total: {points}");
+                        break;
+                    case "EveningGrosbeak":
+                        Console.ForegroundColor = ConsoleColor.Blue;
+                        System.Console.WriteLine($"{sequence} detected! 1000 points added!");
+
+                        points += 1000;
+                        System.Console.WriteLine($"Point total: {points}");
+                        break;
+                    case "GreaterPrairieChicken":
+                        Console.ForegroundColor = ConsoleColor.Blue;
+                        System.Console.WriteLine($"{sequence} detected! 2000 points added!");
+
+                        points += 2000;
+                        System.Console.WriteLine($"Point total: {points}");
+                        break;
+                    case "IcelandGull":
+                        Console.ForegroundColor = ConsoleColor.Blue;
+                        System.Console.WriteLine($"{sequence} detected! 3000 points added!");
+
+                        points += 3000;
+                        System.Console.WriteLine($"Point total: {points}");
+                        break;
+                    case "CrestedIbis":
+                        Console.ForegroundColor = ConsoleColor.Blue;
+                        System.Console.WriteLine($"{sequence} detected! 100 points added!");
+
+                        points += 100;
+                        System.Console.WriteLine($"Point total: {points}");
+                        break;
+                    case "GreatKiskudee":
+                        Console.ForegroundColor = ConsoleColor.Blue;
+                        System.Console.WriteLine($"{sequence} detected! 300 points added!");
+
+                        points += 300;
+                        System.Console.WriteLine($"Point total: {points}");
+                        break;
+                    case "RedCrossbill":
+                        Console.ForegroundColor = ConsoleColor.Blue;
+                        System.Console.WriteLine($"{sequence} detected! 500 points added!");
+
+                        points += 500;
+                        System.Console.WriteLine($"Point total: {points}");
+                        break;
+                    case "Red-neckedPhalarope":
+                        Console.ForegroundColor = ConsoleColor.Blue;
+                        System.Console.WriteLine($"{sequence} detected! 700 points added!");
+
+                        points += 700;
+                        System.Console.WriteLine($"Point total: {points}");
+                        break;
+                    case "Orange-belliedParrot":
+                        Console.ForegroundColor = ConsoleColor.Magenta;
+                        System.Console.WriteLine($"{sequence} detected! 5000 points added!");
+
+                        points += 700;
+                        System.Console.WriteLine($"Point total: {points}");
+                        break;
                     default:
                         break;
                 }
 
-                if (points == 10000)
+                if (points > 10000 && points < 12000)
                 {
+                    Console.ForegroundColor = ConsoleColor.Green;
                     System.Console.WriteLine("10000 points reached! Life has been added!");
                     lives++;
                 }
                 
                 if (lives == 0)
                 {
+                    Console.ForegroundColor = ConsoleColor.Red;
                     System.Console.WriteLine($"Drofsnar has {lives} lives and wasn't able to complete the quest");
                     // "break" is needed to exit out of loop if this case is met!
                     break;
@@ -54,11 +118,11 @@ public class ProgramUI {
 
         // }
 
-        // if (lives > 0)
-        // {
-        //     System.Console.WriteLine($"Drofsnar won with {lives} lives to spare!");
-        //     System.Console.WriteLine($"Final points: {points}");
-        // } else {
-        // }
+        if (lives > 0)
+        {
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            System.Console.WriteLine($"Drofsnar won with {lives} lives to spare!");
+            System.Console.WriteLine($"Final points: {points}");
+        }
     }
 }
